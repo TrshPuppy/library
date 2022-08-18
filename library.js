280721f446874266497befd7a44dd46d6ac3fe72
Rose Pineau <rosepineau@protonmail.com>
2022-08-18 12:20:52 -0400

    Add close span to modal w/ functionality.

--- library.js
+++ library.js
@@ -5,10 +5,16 @@ const removeBookBtn = document.querySelector(".remove-button");
 const addBookBtn = document.querySelector(".add-book");
 const addShelfBtn = document.querySelector(".add-shelf");
 const markReadBtn = document.querySelector(".read-button");
+const modalCloseBtn = document.querySelector(".modal-close");
 
 //Modal
 const modal = document.querySelector(".modal");
 addBookBtn.addEventListener("click", (e) =>
 {
     modal.style.display = "block";
-})
+});
+
+modalCloseBtn.onclick = function()
+{
+    modal.style.display = "none";
+}
