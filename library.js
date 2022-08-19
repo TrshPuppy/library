0754c513d1e39606c7d8ee5ce34751837b114ecb
Rose Pineau <rosepineau@protonmail.com>
2022-08-19 08:52:24 -0400

    Fixed issue where shelf modal not closing on outside click.

--- library.js
+++ library.js
@@ -7,7 +7,7 @@ const addShelfBtn = document.querySelector(".add-shelf");
 const markReadBtn = document.querySelector(".read-button");
 const modalCloseBtns = document.querySelectorAll(".modal-close");
 const modalContentBox = document.querySelector(".modal-content");
-const modalActual = document.querySelector(".modal");
+const modalActuals = document.querySelectorAll(".modal");
 
 //Modals
 // let modalIsOpen = false;
@@ -15,14 +15,12 @@ const modalAddBook = document.querySelector("#modal-add-book");
 addBookBtn.addEventListener("click", (e) =>
 {
     modalAddBook.style.display = "block";
-    
 });
 
 const modalAddShelf = document.querySelector("#modal-add-shelf");
 addShelfBtn.addEventListener("click", (e) =>
 {
     modalAddShelf.style.display = "block";
-    
 });
 
 modalCloseBtns.forEach(button =>
@@ -32,25 +30,33 @@ modalCloseBtns.forEach(button =>
         modalAddShelf.style.display = "none";
     }));
 
-
-modalCloseBtns.onclick = function()
-{
-    modalAddBook.style.display = "none";
-}
-
-
-modalActual.onclick = function(e)
-{
-    if(e.target === modalActual)
+modalActuals.forEach(modalActual =>
+    modalActual.addEventListener('click', (e) =>
     {
-        modalAddBook.style.display = "none";
-        modalAddShelf.style.display = "none";
+        if(e.target === modalActual)
+        {
+            modalAddBook.style.display = "none";
+            modalAddShelf.style.display = "none";
+        }
+        else if(e.target === modalContentBox)
+        {
+            return;
+        }
     }
-    else if(e.target === modalContentBox)
-    {
-        return;
-    }
-}
+    ));
+
+// modalActual.onclick = function(e)
+// {
+//     if(e.target === modalActual)
+//     {
+//         modalAddBook.style.display = "none";
+//         modalAddShelf.style.display = "none";
+//     }
+//     else if(e.target === modalContentBox)
+//     {
+//         return;
+//     }
+// }
 
 // window.onclick = function(e)
 // {
