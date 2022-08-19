75e3e0a0022c33c0adb5baf5f1067cb9817087a6
Rose Pineau <rosepineau@protonmail.com>
2022-08-19 08:18:15 -0400

    Added onclick to modalActual for close modal functionality.

--- library.js
+++ library.js
@@ -6,18 +6,23 @@ const addBookBtn = document.querySelector(".add-book");
 const addShelfBtn = document.querySelector(".add-shelf");
 const markReadBtn = document.querySelector(".read-button");
 const modalCloseBtns = document.querySelectorAll(".modal-close");
+const modalContentBox = document.querySelector(".modal-content");
+const modalActual = document.querySelector(".modal");
 
 //Modals
+// let modalIsOpen = false;
 const modalAddBook = document.querySelector("#modal-add-book");
 addBookBtn.addEventListener("click", (e) =>
 {
     modalAddBook.style.display = "block";
+    
 });
 
 const modalAddShelf = document.querySelector("#modal-add-shelf");
 addShelfBtn.addEventListener("click", (e) =>
 {
     modalAddShelf.style.display = "block";
+    
 });
 
 modalCloseBtns.forEach(button =>
@@ -33,3 +38,24 @@ modalCloseBtns.onclick = function()
     modalAddBook.style.display = "none";
 }
 
+
+modalActual.onclick = function(e)
+{
+    if(e.target !== modalContentBox)
+    {
+        modalAddBook.style.display = "none";
+        modalAddShelf.style.display = "none";
+    }
+}
+
+// window.onclick = function(e)
+// {
+//     if(modalIsOpen === true && e.target != modalContentBox)
+//     {
+//         // modalAddBook.style.display = "none";
+//         // modalAddShelf.style.display = "none";
+
+//         // modalIsOpen = false;
+//         console.log("hi");
+//     }
+// }
